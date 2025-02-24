289d6080ea31bb417347b9a2c2fd86f423d0cbc3
Marina Dmitrieva <70883784+M8r1na@users.noreply.github.com>
2025-02-24 22:05:05 +0300

    fixed getter

--- core/src/engine/task/task_counter.cpp
+++ core/src/engine/task/task_counter.cpp
@@ -136,7 +136,7 @@ void SetLocalTaskCounterData(TaskCounter& counter, std::size_t thread_id) {
     *local_data = {&counter, thread_id};
 }
 
-compiler::ThreadLocal<LocalTaskCounterData>& GetLocalTaskCounterData() {
+compiler::ThreadLocal<LocalTaskCounterData>& GetLocalTaskCounterData() const noexcept {
     return local_task_counter_data;
 }
 
