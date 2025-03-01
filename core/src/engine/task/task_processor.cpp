9544b22c065aee6310e54dcdedddd71ec7decd73
Marina Dmitrieva <70883784+M8r1na@users.noreply.github.com>
2025-03-01 14:31:37 +0300

    .value for optional...

--- core/src/engine/task/task_processor.cpp
+++ core/src/engine/task/task_processor.cpp
@@ -633,7 +633,7 @@ void TaskProcessor::RunEventLoop(const std::size_t thread_index) {
 
             got_task = true;
 
-            impl::TaskContext& context = *(context_ptr.get());
+            impl::TaskContext& context = *(context_ptr.value().get());
             CheckWaitTime(context);
 
             bool has_failed{false};
