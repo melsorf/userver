b9a31cfa007925cf9cf375d018f3322e1550a968
Marina Dmitrieva <70883784+M8r1na@users.noreply.github.com>
2025-03-09 22:30:21 +0300

    wakeup before invalidate

--- core/src/engine/io/fd_control.cpp
+++ core/src/engine/io/fd_control.cpp
@@ -86,20 +86,20 @@ FdControlHolder FdControl::Adopt(int fd) {
 void FdControl::Close() {
     if (!IsValid()) return;
     const auto fd = Fd();
-     Invalidate();
-     if (fd != -1) {
-         if (::close(fd) == -1) {
-             const auto error_code = errno;
-             std::error_code ec(error_code, std::system_category());
-             UASSERT_MSG(!error_code, "Failed to close fd=" + std::to_string(fd));
-             LOG_ERROR() << "Cannot close fd " << fd << ": " << ec.message();
-         }
-     } else {
-         // fd is already -1
-     }
-     read_.WakeupWaiters();
-     write_.WakeupWaiters();
- }
+
+    read_.WakeupWaiters();
+    write_.WakeupWaiters();
+
+    Invalidate();
+    if (fd != -1) {
+        if (::close(fd) == -1) {
+            const auto error_code = errno;
+            std::error_code ec(error_code, std::system_category());
+            UASSERT_MSG(!error_code, "Failed to close fd=" + std::to_string(fd));
+            LOG_ERROR() << "Cannot close fd " << fd << ": " << ec.message();
+        }
+    }
+}
 
 void FdControl::Invalidate() {
     read_.Invalidate();
